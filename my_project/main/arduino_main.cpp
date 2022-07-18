d830cc5614f34f6debd8b5b5945cc22414470e4f
Serialbocks <johnj.gw@gmail.com>
2022-07-18 16:32:15 -0500

    Update esp to allow 4 controllers to connect

--- my_project/main/arduino_main.cpp
+++ my_project/main/arduino_main.cpp
@@ -8,42 +8,57 @@
 
 #include "mappings.h"
 
-#define N64_PIN GPIO_NUM_32
 #define DATA_REQ_PIN GPIO_NUM_23
 #define DATA_READY_PIN GPIO_NUM_22
 #define DATA_PIN GPIO_NUM_21
+#define MAX_GAMEPADS 4
 
-GamepadPtr myGamepad = nullptr;
-int16_t xAxisNeutral = 0;
-int16_t yAxisNeutral = 0;
-int16_t xAxisMin = -300;
-int16_t xAxisMax = 300;
-int16_t yAxisMin = -300;
-int16_t yAxisMax = 300;
+typedef struct gamepad_t {
+  GamepadPtr b32Gamepad = nullptr;
+  int16_t xAxisNeutral = 0;
+  int16_t yAxisNeutral = 0;
+  int16_t xAxisMin = -300;
+  int16_t xAxisMax = 300;
+  int16_t yAxisMin = -300;
+  int16_t yAxisMax = 300;
+} gamepad;
 
-static inline void resetController() {
-  xAxisNeutral = 0;
-  yAxisNeutral = 0;
+uint8_t connectedGamepads = 0;
+gamepad gamepads[MAX_GAMEPADS] = {};
 
-  xAxisMin = -300;
-  xAxisMax = 300;
-  yAxisMin = -300;
-  yAxisMax = 300;
+static inline void resetController(gamepad* gp) {
+  gp->xAxisNeutral = 0;
+  gp->yAxisNeutral = 0;
+
+  gp->xAxisMin = -300;
+  gp->xAxisMax = 300;
+  gp->yAxisMin = -300;
+  gp->yAxisMax = 300;
 }
 
 void onConnectedGamepad(GamepadPtr gp) {
-    myGamepad = gp;
-    BP32.enableNewBluetoothConnections(false);
-    resetController();
+    if(connectedGamepads < MAX_GAMEPADS) {
+      gamepad* gamepad = &gamepads[connectedGamepads];
+      gamepad->b32Gamepad = gp;
+      resetController(gamepad);
+      connectedGamepads++;
+    }
+
+    if(connectedGamepads >= MAX_GAMEPADS) {
+      BP32.enableNewBluetoothConnections(false);
+    }
+
 }
 
 void onDisconnectedGamepad(GamepadPtr gp) {
-    myGamepad = nullptr;
+    connectedGamepads--;
+    gamepads[connectedGamepads].b32Gamepad = nullptr;
     BP32.enableNewBluetoothConnections(true);
 }
 
-static inline uint32_t get_controller_state() {
+static inline uint32_t get_controller_state(gamepad* gp) {
     uint32_t n64_buttons = 0;
+    GamepadPtr myGamepad = gp->b32Gamepad;
 
     BP32.update();
     if (myGamepad && myGamepad->isConnected()) {
@@ -95,17 +110,17 @@ static inline uint32_t get_controller_state() {
         n64_buttons++;
 
       // analog sticks
-      if(xAxis > xAxisMax)
-        xAxisMax = xAxis;
-      if(xAxis < xAxisMin)
-        xAxisMin = xAxis;
-      if(yAxis > yAxisMax)
-        yAxisMax = yAxis;
-      if(yAxis < yAxisMin)
-        yAxisMin = yAxis;
+      if(xAxis > gp->xAxisMax)
+        gp->xAxisMax = xAxis;
+      if(xAxis < gp->xAxisMin)
+        gp->xAxisMin = xAxis;
+      if(yAxis > gp->yAxisMax)
+        gp->yAxisMax = yAxis;
+      if(yAxis < gp->yAxisMin)
+        gp->yAxisMin = yAxis;
 
       bool xAxisPositive = xAxis > 0;
-      float n64XAxisFactor =xAxisPositive ? ((float)xAxis / xAxisMax) : ((float)xAxis / xAxisMin);
+      float n64XAxisFactor =xAxisPositive ? ((float)xAxis / gp->xAxisMax) : ((float)xAxis / gp->xAxisMin);
       uint8_t n64XAxis = 0;
       if(n64XAxisFactor > DEADZONE) {
         n64XAxis = (uint8_t)(n64XAxisFactor * 110);
@@ -118,7 +133,7 @@ static inline uint32_t get_controller_state() {
       }
 
       bool yAxisPositive = yAxis > 0;
-      float n64YAxisFactor = yAxisPositive ? ((float)yAxis / yAxisMax) : ((float)yAxis / yAxisMin);
+      float n64YAxisFactor = yAxisPositive ? ((float)yAxis / gp->yAxisMax) : ((float)yAxis / gp->yAxisMin);
       uint8_t n64YAxis = 0;
       if(n64YAxisFactor > DEADZONE) {
         n64YAxis = (uint8_t)(n64YAxisFactor * 110);
@@ -142,7 +157,6 @@ static inline uint32_t get_controller_state() {
 void setup() {
     BP32.setup(&onConnectedGamepad, &onDisconnectedGamepad);
     BP32.forgetBluetoothKeys();
-    gpio_set_direction(N64_PIN, GPIO_MODE_INPUT_OUTPUT);
     gpio_set_direction(DATA_REQ_PIN, GPIO_MODE_INPUT);
     gpio_set_direction(DATA_READY_PIN, GPIO_MODE_OUTPUT);
     gpio_set_direction(DATA_PIN, GPIO_MODE_OUTPUT);
@@ -151,18 +165,26 @@ void setup() {
 // Arduino loop function. Runs in CPU 1
 void loop() {
     while(!gpio_get_level(DATA_REQ_PIN));
-    uint32_t buttons = get_controller_state();
-    
-    int dataCount = 32;
-    while(dataCount) {
-        gpio_set_level(DATA_PIN, !!(buttons & 0x80000000));
-        gpio_set_level(DATA_READY_PIN, 1);
-        delay2us();
-        delay2us();
-        gpio_set_level(DATA_READY_PIN, 0);
-        delay2us();
-        delay2us();
-        dataCount--;
-        buttons <<= 1;
+    uint32_t buttonArr[MAX_GAMEPADS] = {};
+
+    for(int i = 0; i < connectedGamepads; i++) {
+      buttonArr[i] = get_controller_state(&(gamepads[i]));
     }
+    
+    for(int i = 0; i < MAX_GAMEPADS; i++) {
+          int dataCount = 32;
+          uint32_t buttons = buttonArr[i];
+          while(dataCount) {
+              gpio_set_level(DATA_PIN, !!(buttons & 0x80000000));
+              gpio_set_level(DATA_READY_PIN, 1);
+              delay2us();
+              delay2us();
+              gpio_set_level(DATA_READY_PIN, 0);
+              delay2us();
+              delay2us();
+              dataCount--;
+              buttons <<= 1;
+          }
+    }
+
 }
