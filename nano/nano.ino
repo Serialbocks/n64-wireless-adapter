e0bc873bf7a61499883369155427ebec9081702a
Serialbocks <johnj.gw@gmail.com>
2022-07-18 17:59:34 -0500

    Update nano to support 4 controllers (untested)

--- nano/nano.ino
+++ nano/nano.ino
@@ -3,30 +3,53 @@
  * 
 */
 
+#define PIN0_MASK 0b00000001
+#define PIN1_MASK 0b00000010
 #define PIN2_MASK 0b00000100
 #define PIN3_MASK 0b00001000
 #define PIN4_MASK 0b00010000
 #define PIN5_MASK 0b00100000
 #define PIN6_MASK 0b01000000
+#define PIN7_MASK 0b10000000
 
-#define N64_PIN PIN6_MASK
-#define DATA_PIN PIN3_MASK
-#define DATA_READY_PIN PIN4_MASK
-#define DATA_REQ_PIN PIN5_MASK
+#define N64_CONT_1_DATA PIN0_MASK
+#define N64_CONT_1_ACTIVE PIN1_MASK
+#define N64_CONT_2_DATA PIN2_MASK
+#define N64_CONT_2_ACTIVE PIN3_MASK
+#define N64_CONT_3_DATA PIN4_MASK
+#define N64_CONT_3_ACTIVE PIN5_MASK
+#define N64_CONT_4_DATA PIN6_MASK
+#define N64_CONT_4_ACTIVE PIN7_MASK
+
+#define DATA_PIN PIN2_MASK
+#define DATA_READY_PIN PIN3_MASK
+#define DATA_REQ_PIN PIN4_MASK
 
 #define CONTROLLER_ID 0x0502
+#define MAX_GAMEPADS 4
 
 // MACROS
-#define gpio_read(PIN) ( !!( PIND & PIN ) ) // 1 cycle
-#define gpio_write(PIN, val) ( (val) ? PORTD |= PIN : PORTD &= ~PIN )
-#define gpio_set_input(PIN) DDRD &= ~PIN; PORTD |= PIN;
-#define gpio_set_output(PIN) DDRD |= PIN; PORTD |= PIN;
+#define gpio_b_read(PIN) ( !!( PINB & PIN ) ) // 1 cycle
+#define gpio_b_write(PIN, val) ( (val) ? PORTB |= PIN : PORTB &= ~PIN )
+#define gpio_b_set_input(PIN) DDRB &= ~PIN; PORTB |= PIN;
+#define gpio_b_set_output(PIN) DDRB |= PIN; PORTB |= PIN;
+
+#define gpio_c_read(PIN) ( !!( PINC & PIN ) ) // 1 cycle
+#define gpio_c_write(PIN, val) ( (val) ? PORTC |= PIN : PORTC &= ~PIN )
+#define gpio_c_set_input(PIN) DDRC &= ~PIN; PORTC |= PIN;
+#define gpio_c_set_output(PIN) DDRC |= PIN; PORTC |= PIN;
+
+#define gpio_d_read(PIN) ( !!( PIND & PIN ) ) // 1 cycle
+#define gpio_d_write(PIN, val) ( (val) ? PORTD |= PIN : PORTD &= ~PIN )
+#define gpio_d_set_input(PIN) DDRD &= ~PIN; PORTD |= PIN;
+#define gpio_d_set_output(PIN) DDRD |= PIN; PORTD |= PIN;
+
 
 #define delay1() asm("nop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\n"); // delays ~1 microsecond. Not exactly 16 no-ops, because we need some cycles to do other things
 #define delayHalf() asm("nop\nnop\nnop\n");
 #define delay2() delay1() delay1();
 
-uint32_t buttons = 0;
+uint32_t buttonArr[MAX_GAMEPADS] = {};
 
 void setup() {
   // Set TCNT1 to use system clock with no divide
@@ -37,30 +60,67 @@ void setup() {
   asm("cli\n");
 
   // N64 pin init
-  gpio_set_input(N64_PIN);
+  gpio_c_set_input(N64_CONT_1_DATA);
+  gpio_c_set_input(N64_CONT_1_ACTIVE);
+  gpio_c_set_input(N64_CONT_2_DATA);
+  gpio_c_set_input(N64_CONT_2_ACTIVE);
+  gpio_c_set_input(N64_CONT_3_DATA);
+  gpio_c_set_input(N64_CONT_3_ACTIVE);
+  gpio_c_set_input(N64_CONT_4_DATA);
+  gpio_c_set_input(N64_CONT_4_ACTIVE);
 
   // Data pins init
-  gpio_set_output(DATA_REQ_PIN);
-  gpio_write(DATA_REQ_PIN, 0);
-  gpio_set_input(DATA_READY_PIN);
-  gpio_set_input(DATA_PIN);
-
+  gpio_d_set_output(DATA_REQ_PIN);
+  gpio_d_write(DATA_REQ_PIN, 0);
+  gpio_d_set_input(DATA_READY_PIN);
+  gpio_d_set_input(DATA_PIN);
 
 }
 
 void loop() {
+  uint8_t n64DataPin = 0;
+  uint8_t n64ActivePin = 0;
   uint8_t bitCount = 8;
   uint8_t dataIn = 0;
-  while(!gpio_read(N64_PIN)); // Console off
-  while(gpio_read(N64_PIN)); // Idle
+  uint32_t buttons = 0;
+  for(int i = MAX_GAMEPADS - 1; i >= 0; i--) {
+    switch(i) {
+      case 0:
+        n64DataPin = N64_CONT_1_DATA;
+        n64ActivePin = N64_CONT_1_ACTIVE;
+        break;
+      case 1:
+        n64DataPin = N64_CONT_2_DATA;
+        n64ActivePin = N64_CONT_2_ACTIVE;
+        break;
+      case 2:
+        n64DataPin = N64_CONT_3_DATA;
+        n64ActivePin = N64_CONT_3_ACTIVE;
+        break;
+      case 3:
+        n64DataPin = N64_CONT_4_DATA;
+        n64ActivePin = N64_CONT_4_ACTIVE;
+        break;
+      default:
+        break;
+    }
+
+    if(!gpio_c_read(n64ActivePin)) {
+      // This controller is not plugged in
+      continue;
+    }
+    
+    bitCount = 8;
+    dataIn = 0;
+      while(gpio_c_read(n64DataPin)); // Idle
 
   read_console_command:
     dataIn <<= 1;
     delay1();
-    buttons += gpio_read(N64_PIN);
+    dataIn += gpio_c_read(n64DataPin);
     bitCount--;
-    while(!gpio_read(N64_PIN));
-    while(gpio_read(N64_PIN));
+    while(!gpio_c_read(n64DataPin));
+    while(gpio_c_read(n64DataPin));
 
     if(bitCount)
       goto read_console_command;
@@ -70,49 +130,52 @@ void loop() {
       buttons = CONTROLLER_ID;
     } else {
       bitCount = 32;
+      buttons = buttonArr[i];
     }
-
-  gpio_set_output(N64_PIN);
-  gpio_write(N64_PIN, 1);
-  delay2();
-  delay2();
-  delay2();
+    gpio_c_set_output(n64DataPin);
+    gpio_c_write(n64DataPin, 1);
+    delay2();
+    delay2();
+    delay2();
   
   sendBits:
-    gpio_write(N64_PIN, 0);
+    gpio_c_write(n64DataPin, 0);
     delayHalf();
-    gpio_write(N64_PIN, buttons & 0x80000000);
+    gpio_c_write(n64DataPin, buttons & 0x80000000);
     delay2();
-    gpio_write(N64_PIN, 1);
+    gpio_c_write(n64DataPin, 1);
     //delay1();
     bitCount--;
     buttons <<= 1;
     if(bitCount)
       goto sendBits;
 
-  gpio_write(N64_PIN, 0);
-  delay2();
-  gpio_write(N64_PIN, 1);
-  delay1();
-
-  gpio_set_input(N64_PIN);
+    gpio_c_write(n64DataPin, 0);
+    delay2();
+    gpio_c_write(n64DataPin, 1);
+    delay1();
+  
+    gpio_c_set_input(n64DataPin);
+  }
 
   // Request and read data from esp32
-  buttons = 0;
-  bitCount = 32;
-  gpio_write(DATA_REQ_PIN, 1);
-  while(bitCount) {
-    while(!gpio_read(DATA_READY_PIN));
-    buttons <<= 1;
-    buttons += gpio_read(DATA_PIN);
-    gpio_write(DATA_REQ_PIN, 0);
-    while(gpio_read(DATA_READY_PIN));
-    bitCount--;
+  for(int i = 0; i < MAX_GAMEPADS; i++) {
+    buttons = 0;
+    bitCount = 32;
+    gpio_d_write(DATA_REQ_PIN, 1);
+    while(bitCount) {
+      uint32_t count = 0;
+      while(!gpio_d_read(DATA_READY_PIN) && count++ < 5000);
+      buttons <<= 1;
+      buttons += gpio_d_read(DATA_PIN);
+      gpio_d_write(DATA_REQ_PIN, 0);
+
+      if(count >= 5000) return;
+      while(gpio_d_read(DATA_READY_PIN));
+      bitCount--;
+    }
+    buttonArr[i] = buttons;
   }
 
   
-  bitCount = 32;
-
-
-
 }
